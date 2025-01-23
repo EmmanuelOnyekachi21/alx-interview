c5228699619a76fa440724e2ee132eb251027dd8
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2025-01-23 14:07:24 +0100

    Added Readme and All taskes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,57 +1,79 @@
 #!/usr/bin/python3
-"""
-Read stdin line by line and computes metrics
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-<status code> <file size>, skip line if not this format
-After every 10minutes or keyboard interrupt (CTRL + C)
-print these from beginning: number of lines by status code
-possible status codes: 200, 301, 400, 401, 404, 405, and 500
-if status code isn't an integer, do not print it
-format: <status code>: <number>
-Status code must be printed in ascending order
-"""
+"""Parses a log file from stdin following a given format"""
+
 import sys
+from datetime import datetime
 
 
-def print_msg(codes, file_size):
-    print("File size: {}".format(file_size))
-    for key, val in sorted(codes.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+def validate_line(line):
+    """Validates the give string adheres toprovided parsing format."""
+
+    try:
+        ip_addr, remainder = line.split(' ', 1)
+        excess, remainder = remainder.split('[', 1)
+        date_str, remainder = remainder.split(']', 1)
+        excess, remainder = remainder.split('"', 1)
+        url_given, remainder = remainder.split('"', 1)
+        status, file_size = remainder.strip().split(' ')
+    except Exception:
+        return False
+
+    for num in ip_addr.split('.'):
+        try:
+            int(num)
+        except Exception:
+            return False
+    try:
+        datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S.%f")
+    except Exception:
+        return False
+    if url_given != "GET /projects/260 HTTP/1.1":
+        return False
+    try:
+        int(status)
+    except Exception:
+        return False
+    try:
+        file_sz = int(file_size)
+    except Exception:
+        return False
+    return (status, file_sz)
 
 
-file_size = 0
-code = 0
-count_lines = 0
-codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
+def print_stat(file_size, my_status):
+    """ Prints the log statistics."""
+    print(f"File size: {file_size}")
+    for key, value in my_status.items():
+        if value > 0:
+            print(f"{key}: {value}")
+
+
+read_lines = 0
+total_file_size = 0
+my_status = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
 }
-
 try:
     for line in sys.stdin:
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]
-
-        if len(parsed_line) > 2:
-            count_lines += 1
-
-            if count_lines <= 10:
-                file_size += int(parsed_line[0])
-                code = parsed_line[1]
-
-                if (code in codes.keys()):
-                    codes[code] += 1
-
-            if (count_lines == 10):
-                print_msg(codes, file_size)
-                count_lines = 0
-
-finally:
-    print_msg(codes, file_size)
+        read_lines += 1
+        line.strip()
+        if not validate_line(line):
+            continue
+        status, file_size = validate_line(line)
+        total_file_size += file_size
+        for key in my_status.keys():
+            if status == key:
+                my_status[key] += 1
+                break
+        if read_lines % 10 == 0:
+            print_stat(total_file_size, my_status)
+    print_stat(total_file_size, my_status)
+except KeyboardInterrupt:
+    print_stat(total_file_size, my_status)
